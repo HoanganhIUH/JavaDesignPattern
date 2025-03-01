a55c68167a7e188c4f7ea61ea5ab779187ca4234
HoanganhIUH <21015461.anh@student.iuh.edu.vn>
2025-03-01 08:12:34 +0700

    Add class ThreadSafeSingleton and img ThreadSafeSingleton

--- src/Singletonpattern/Main.java
+++ src/Singletonpattern/Main.java
@@ -2,15 +2,16 @@ package Singletonpattern;
 
 public class Main {
 	public static void main(String[] args) {
-		EagerInitializedSingleton instance1 = EagerInitializedSingleton.getInstance();
-		EagerInitializedSingleton instance2 = EagerInitializedSingleton.getInstance();
-		System.out.println("Instance 1 hash:" + instance1.hashCode());
-		System.out.println("Instance 2 hash:" + instance2.hashCode());
+//		EagerInitializedSingleton instance1 = EagerInitializedSingleton.getInstance();
+//		EagerInitializedSingleton instance2 = EagerInitializedSingleton.getInstance();
+//		System.out.println("Instance 1 hash:" + instance1.hashCode());
+//		System.out.println("Instance 2 hash:" + instance2.hashCode());
 
-//		ThreadSafeSingleton instance3 = ThreadSafeSingleton.getInstance();
-//		ThreadSafeSingleton instance4 = ThreadSafeSingleton.getInstance();
-//		System.out.println("Instance 3 hash:" + instance3.hashCode());
-//		System.out.println("Instance 4 hash:" + instance4.hashCode());
+        ThreadSafeSingleton instance3 = ThreadSafeSingleton.getInstance();
+        ThreadSafeSingleton instance4 = ThreadSafeSingleton.getInstance();
+        System.out.println("Instance 3 hash:" + instance3.hashCode());
+        System.out.println("Instance 4 hash:" + instance4.hashCode());
+        
 	}
 	
 }
