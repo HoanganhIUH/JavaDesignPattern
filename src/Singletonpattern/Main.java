6e810849c8df682b9fa4e81442d636c993102a4c
HoanganhIUH <21015461.anh@student.iuh.edu.vn>
2025-03-01 08:09:38 +0700

    Create folder MinhChung and Add img EagerInitializedSingleton

--- src/Singletonpattern/Main.java
+++ src/Singletonpattern/Main.java
@@ -7,5 +7,10 @@ public class Main {
 		System.out.println("Instance 1 hash:" + instance1.hashCode());
 		System.out.println("Instance 2 hash:" + instance2.hashCode());
 
+//		ThreadSafeSingleton instance3 = ThreadSafeSingleton.getInstance();
+//		ThreadSafeSingleton instance4 = ThreadSafeSingleton.getInstance();
+//		System.out.println("Instance 3 hash:" + instance3.hashCode());
+//		System.out.println("Instance 4 hash:" + instance4.hashCode());
 	}
+	
 }
