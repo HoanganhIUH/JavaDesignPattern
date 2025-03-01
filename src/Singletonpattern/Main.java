c44f9f7ad984196e14d87158952689e11c5c572e
HoanganhIUH <21015461.anh@student.iuh.edu.vn>
2025-03-01 08:19:21 +0700

    Add class StaticBlockSingleton and img StaticBlockSingleton

--- src/Singletonpattern/Main.java
+++ src/Singletonpattern/Main.java
@@ -7,10 +7,15 @@ public class Main {
 //		System.out.println("Instance 1 hash:" + instance1.hashCode());
 //		System.out.println("Instance 2 hash:" + instance2.hashCode());
 
-        ThreadSafeSingleton instance3 = ThreadSafeSingleton.getInstance();
-        ThreadSafeSingleton instance4 = ThreadSafeSingleton.getInstance();
-        System.out.println("Instance 3 hash:" + instance3.hashCode());
-        System.out.println("Instance 4 hash:" + instance4.hashCode());
+//        ThreadSafeSingleton instance3 = ThreadSafeSingleton.getInstance();
+//        ThreadSafeSingleton instance4 = ThreadSafeSingleton.getInstance();
+//        System.out.println("Instance 3 hash:" + instance3.hashCode());
+//        System.out.println("Instance 4 hash:" + instance4.hashCode());
+        
+        StaticBlockSingleton instance5 = StaticBlockSingleton.getInstance();
+        StaticBlockSingleton instance6 = StaticBlockSingleton.getInstance();
+        System.out.println("Instance 5 hash:" + instance5.hashCode());
+        System.out.println("Instance 6 hash:" + instance6.hashCode());
         
 	}
 	
