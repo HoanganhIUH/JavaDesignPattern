a8a5c7692235bca226865e0a5e4f6359452c60e2
HoanganhIUH <21015461.anh@student.iuh.edu.vn>
2025-04-16 17:31:04 +0700

    Create Project

---
+++ Buoi06_Microservice_Bai01/customer-serivce/src/main/java/com/example/customer_serivce/CustomerSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_serivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSerivceApplication.class, args);
+	}
+
+}
---
+++ Buoi06_Microservice_Bai01/customer-serivce/src/test/java/com/example/customer_serivce/CustomerSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customer_serivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
